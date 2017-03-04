f3236a75fcd6e54653b12a75a8c234f93a9f1486
Stefan <cobrijani@gmail.com>
2017-03-04 01:30:46 +0100

    Pdf controller and package info

--- src/main/java/rs/ac/uns/ftn/controllers/AktPdfController.java
+++ src/main/java/rs/ac/uns/ftn/controllers/AktPdfController.java
@@ -1,7 +1,47 @@
 package rs.ac.uns.ftn.controllers;
 
+import com.itextpdf.text.DocumentException;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+import rs.ac.uns.ftn.services.AktService;
+import rs.ac.uns.ftn.util.XMLUtil;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+
 /**
  * Created by SBratic on 3/2/2017.
  */
+@RestController
+@RequestMapping(value = "/api/v1/akti", produces = MediaType.APPLICATION_PDF_VALUE)
 public class AktPdfController {
+
+  private final AktService aktService;
+
+  public AktPdfController(AktService aktService) {
+    this.aktService = aktService;
+  }
+
+
+  @GetMapping(value = "/{id}")
+  public void getOne(@PathVariable String id, HttpServletResponse response) {
+    Document document = aktService.findById(id, Document.class);
+    try {
+      XMLUtil.toPdf(document, response.getOutputStream(), "src/main/resources/xslt/pdf/akt-pdf.xsl");
+      response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+      response.flushBuffer();
+    } catch (TransformerException | IOException | SAXException e) {
+      e.printStackTrace();
+    }
+  }
+
+
+
+
 }
--- src/main/java/rs/ac/uns/ftn/model/generated/package-info.java
+++ src/main/java/rs/ac/uns/ftn/model/generated/package-info.java
@@ -1,4 +1,11 @@
-/**
- * Created by SBratic on 3/4/2017.
- */
+@javax.xml.bind.annotation.XmlSchema(
+  xmlns = {
+    @javax.xml.bind.annotation.XmlNs(prefix = "akt", namespaceURI = "http://parlament.gov.rs/rs.ac.uns.ftn.model.akt"),
+    @javax.xml.bind.annotation.XmlNs(prefix = "meta", namespaceURI = "http://parlament.gov.rs/rs.ac.uns.ftn.model.metadata"),
+    @javax.xml.bind.annotation.XmlNs(prefix = "am", namespaceURI = "http://parlament.gov.rs/rs.ac.uns.ftn.model.amandman")
+  },
+  elementFormDefault = XmlNsForm.QUALIFIED
+)
 package rs.ac.uns.ftn.model.generated;
+
+import javax.xml.bind.annotation.XmlNsForm;
